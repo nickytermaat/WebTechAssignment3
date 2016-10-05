356261c293175f55085d4bd0e7c5603787bef28a
Kris <krisscholte@hotmail.com>
2016-10-05 15:10:01 +0200

    Second setup

--- index.js
+++ index.js
@@ -3,29 +3,42 @@
  */
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+
+//Connect to the database
 mongoose.connect('mongodb://localhost/test');
 var Schema = mongoose.Schema;
+var Movie = require('MovieSchema.js');
+var User = require('userSchema.js');
+var router = require('jsonToken.js');
 
-
-var bodyParser = require('body-parser');
+//Register bodyparser
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-app.get('/', function (req, res) {
-    res.send("Hello, World!");
-});
-app.listen(3000, function(){
-    console.log("Example app on port 3000");
-});
-app.get('/test', function(req, res){
-   res.send("Hoi test werkt");
+//Set and start the server
+var Server =
+    app.listen(3000, function(){
+    console.log("Example app on port" + Server.address().port);
+    var user1 = new User({name: 'Kris'});
+    user1.save(function(err, result){
+        if(err){
+            return console.error(err);
+        }
+    });
+
+    console.log("test");
 });
 
-app.get('/movies', function(req, res) {
-    res.send("Displaylist of movies");
+app.get('list', function(req, res){
+   var token = req.headers['authorization'];
 
+    jwt.verify(token, app.get('private-key'), function(error, decoded){
+        if(err){
+            //Something went wrong
+        } else {
+            //Do something with the decoded parameter
+        }
+    })
 });
-app.get('/movies/*', function(req, res) {
-    res.send("Display movie with id: " +req.params[0]);
-});
-
